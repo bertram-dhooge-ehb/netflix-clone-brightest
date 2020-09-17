89014b4c43bb0a1281038d832cf70083d34d49da
bertram-dhooge-ehb <bertram.dhooge@student.ehb.be>
2020-09-17 15:18:44 +0200

    Row of movies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import Gallery from "./Gallery.js";
+import Gallery from "./Gallery";
+import Row from "./Row";
+import requests from "./requests";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Test</h1>
-      <Gallery></Gallery>
+      <Gallery />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
